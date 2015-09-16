ea801a380536644154ed50fd3ae6b1888ec961fd
Mike Wilson <Mike.Wilson@BetVictor.com>
2015-09-16 10:05:33 +0200

    FilmOn Recording Down loader Initial Version

---
+++ FilmOnDL/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Text1 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TxtUserName = New System.Windows.Forms.TextBox()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(32, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Fetch"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Text1
+        '
+        Me.Text1.AcceptsReturn = True
+        Me.Text1.BackColor = System.Drawing.Color.LightGray
+        Me.Text1.Location = New System.Drawing.Point(32, 81)
+        Me.Text1.Multiline = True
+        Me.Text1.Name = "Text1"
+        Me.Text1.Size = New System.Drawing.Size(736, 154)
+        Me.Text1.TabIndex = 1
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(32, 58)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(74, 17)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "Download"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(653, 13)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(171, 58)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(120, 17)
+        Me.CheckBox2.TabIndex = 4
+        Me.CheckBox2.Text = "Delete Downloaded"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TxtUserName
+        '
+        Me.TxtUserName.Location = New System.Drawing.Point(518, 15)
+        Me.TxtUserName.Name = "TxtUserName"
+        Me.TxtUserName.Size = New System.Drawing.Size(100, 20)
+        Me.TxtUserName.TabIndex = 5
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Location = New System.Drawing.Point(518, 42)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TxtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.TxtPassword.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(454, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "UserName"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(457, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Password"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(801, 262)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.TxtUserName)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Text1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "FilmOn Downloader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Text1 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents TxtUserName As TextBox
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ FilmOnDL/Form1.vb
@@ -0,0 +1,147 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Dim SessionKey As String
+
+    Private Function SendRequest(uri As Uri, jsonDataBytes As Byte(), contentType As String, method As String) As String
+        'Post JSON to URL and get response
+        Dim req As HttpWebRequest = WebRequest.Create(uri)
+        req.ContentType = contentType
+        req.Method = method
+        req.ContentLength = jsonDataBytes.Length
+        req.UserAgent = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0"
+
+        Dim stream = req.GetRequestStream()
+        stream.Write(jsonDataBytes, 0, jsonDataBytes.Length)
+        stream.Close()
+
+        Dim response = req.GetResponse().GetResponseStream()
+
+
+        Dim reader As New StreamReader(response)
+        Dim res = reader.ReadToEnd()
+        reader.Close()
+        response.Close()
+
+        Return res
+    End Function
+
+    Public Shared Function GenerateMd5Hash(input As String) As String
+        Dim x = New System.Security.Cryptography.MD5CryptoServiceProvider()
+        Dim computeHash = System.Text.Encoding.UTF8.GetBytes(input)
+        computeHash = x.ComputeHash(computeHash)
+        Dim stringBuilder = New System.Text.StringBuilder(computeHash.Length)
+        For i As Int16 = 0 To computeHash.Length - 1
+            stringBuilder.Append(computeHash(i).ToString("x2"))
+        Next
+        Return stringBuilder.ToString()
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Send Init Command
+        Dim data = Encoding.UTF8.GetBytes("{""app_id"" : ""foo"",""app_secret"" : ""bar""}")
+        Dim FilmOn As New Uri("http://www.filmon.com/tv/api/init")
+        Dim response As String = SendRequest(FilmOn, data, "application/json", "POST")
+        Dim UserPass As String = GenerateMd5Hash(TxtPassword.Text)
+        Dim P1 As Integer = 0
+        Dim P2 As Integer
+        Dim RecTitle As String
+        Dim RecStatus As String
+        Dim RecLink As String
+        Dim RecDate As String
+        Dim filepath As String
+        Dim DelResponse As String
+
+        Try
+
+            'Get the Session Key
+            SessionKey = Mid$(response, InStr(response, "session_key") + 14, 32)
+
+
+            'Log In User
+            Dim LogIn As New Uri("http://www.filmon.com/tv/api/login?session_key=" + SessionKey)
+            data = Encoding.UTF8.GetBytes("{""login"" : """ + TxtUserName.Text + """,""password"" : """ + UserPass + """}")
+            response = SendRequest(LogIn, data, "application/json", "POST")
+
+            'Get recording list for user
+            Dim DVRList As New Uri("http://www.filmon.com/tv/api/dvr/list?session_key=" + SessionKey)
+            data = Encoding.UTF8.GetBytes("")
+
+            response = SendRequest(DVRList, data, "application/json", "POST")
+
+            While P1 <> -1
+                ' Get the recording title
+                P1 = response.IndexOf(",""title"":""", P1)
+                If P1 <> -1 Then
+                    RecTitle = Mid$(response, P1 + 11, InStr(Mid$(response, P1 + 12), """"))
+                    ' Get the recording status
+                    P1 = response.IndexOf(",""status"":""", P1)
+                    RecStatus = Mid$(response, P1 + 12, InStr(Mid$(response, P1 + 13), """"))
+
+                    If RecStatus = "Recorded" Then
+                        ' Get the recording url
+                        P1 = response.IndexOf(",""download_link"":""", P1)
+                        RecLink = Mid$(response, P1 + 19, InStr(Mid$(response, P1 + 20), """"))
+
+                        P2 = RecLink.IndexOf(".mp4") - 18
+                        ' Get recording date
+                        RecDate = Mid(RecLink, P2, 10)
+                        ' Format save file name
+                        RecStatus = RecTitle & " " & Mid(RecDate, 9, 2) & "-" & Mid(RecDate, 6, 2) & "-" & Mid(RecDate, 3, 2) & ".mp4"
+                        ' Create Linux download command: wget http:\/\/s3.dvr.gv.filmon.com\/schdld\/14\/36\/248\/2015.09.06\/2632049.mp4 -O Lady Chatterley's Lover.mp4
+                        Text1.Text = Text1.Text & "wget " & RecLink & " -O """ & RecTitle & " " & Mid(RecDate, 9, 2) & "-" & Mid(RecDate, 6, 2) & "-" & Mid(RecDate, 3, 2) & ".mp4""" & Environment.NewLine
+
+                        If CheckBox1.Checked Then
+                            RecLink = Regex.Replace(RecLink, "\\", "")
+                            RecLink = "http://hls" + Mid(RecLink, 10)
+                            ' Download the recording
+                            filepath = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyPictures, "FilmOn\" & RecStatus)
+                            My.Computer.Network.DownloadFile(RecLink, filepath, "", "", True, 50000, True)
+
+                            If CheckBox2.Checked Then
+                                ' remove recording from DVR list
+                                data = Encoding.UTF8.GetBytes("{""record_id"" : """ + Mid(RecLink, P2 + 5, 7) + """}")
+                                Dim DVRRemove As New Uri("http://www.filmon.com/tv/api/dvr/remove?session_key=" + SessionKey)
+                                DelResponse = SendRequest(DVRRemove, data, "application/json", "POST")
+                                MsgBox(DelResponse)
+                            End If
+
+                        End If
+
+                    End If
+
+                End If
+
+            End While
+
+        Catch ex As Exception
+
+            MsgBox("Error" + ex.Message)
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        My.Settings.UserName = TxtUserName.Text
+        My.Settings.Password = TxtPassword.Text
+
+
+        Application.Exit() 'Exit
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        TxtUserName.Text = My.Settings.UserName
+        TxtPassword.Text = My.Settings.Password
+
+    End Sub
+End Class
